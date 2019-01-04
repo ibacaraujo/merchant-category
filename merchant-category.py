9053a05a6a4c82bf623d69a7a8dee41cfce8662b
Iago Breno <ibacaraujo@gmail.com>
2019-01-04 00:12:44 -0200

    Update merchant-category.py

--- merchant-category.py
+++ merchant-category.py
@@ -1,2 +1,19 @@
 # Merchant Category Recommendation
 # Flexibility, boy, and action.
+
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+
+import os
+print(os.listdir("../input"))
+
+train = pd.read_csv('../input/train.csv', parse_dates=['first_active_month'])
+test = pd.read_csv('../input/test.csv', parse_dates=['first_active_month'])
+
+data_dictionary = pd.read_excel('../input/Data_Dictionary.xlsx', sheet_name='train')
+
+train['feature_1'] = train['feature_1'].astype('category')
+train['feature_2'] = train['feature_2'].astype('category')
+train['feature_3'] = train['feature_3'].astype('category')
+
+train.info()
