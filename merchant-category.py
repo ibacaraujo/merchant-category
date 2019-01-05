78454a4184e4dd3662c7d459ea66c5e07d16631e
Iago Breno <ibacaraujo@gmail.com>
2019-01-05 04:16:08 -0200

    Add bar plots

--- merchant-category.py
+++ merchant-category.py
@@ -3,6 +3,7 @@
 
 import numpy as np # linear algebra
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import matplotlib.pyplot as plt
 
 import os
 print(os.listdir("../input"))
@@ -17,3 +18,8 @@ train['feature_2'] = train['feature_2'].astype('category')
 train['feature_3'] = train['feature_3'].astype('category')
 
 train.info()
+
+fig, ax = plt.subplots(1, 3, figsize=(16,6))
+train['feature_1'].value_counts().sort_index().plot(kind='bar', ax=ax[0], color='teal', title='feature_1');
+train['feature_2'].value_counts().sort_index().plot(kind='bar', ax=ax[1], color='brown', title='feature_2');
+train['feature_3'].value_counts().sort_index().plot(kind='bar', ax=ax[2], color='gold', title='feature_3')
